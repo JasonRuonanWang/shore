681659f40fc0a3565290dfbab1af51db1bb69657
Jason Wang <jason.wang@icrar.org>
2016-06-12 00:46:01 +0800

    changed magnus run.sh

--- hpcwrappers/submit.magnus.sh
+++ hpcwrappers/submit.magnus.sh
@@ -3,11 +3,15 @@
 #sbatch --account=$IVEC_PROJECT --time=00:01:00 --nodes=4 --ntasks-per-node=1 -p debugq run.sh
 
 
-for i in $(seq 20 -1 1)
+for i in $(seq 100 -10 15)
 do
-	sbatch --account=$IVEC_PROJECT --time=02:00:00 --nodes=$i --ntasks-per-node=1 --dependency=singleton run.sh
+	sbatch --account=$IVEC_PROJECT --time=01:00:00 --nodes=$i --ntasks-per-node=1 --dependency=singleton run.magnus.sh
 done
 
+for i in $(seq 10 -1 1)
+do
+	sbatch --account=$IVEC_PROJECT --time=02:00:00 --nodes=$i --ntasks-per-node=1 --dependency=singleton run.magnus.sh
+done
 
 
 
